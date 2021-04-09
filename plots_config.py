05e3de48a73e1a3f1459399db5c15698f1b1468f
smilg <jacobasmilg@gmail.com>
2021-04-09 01:33:25 -0400

    Move more layout config options into the file, notably for the kamada-kawai viz

--- plots_config.py
+++ plots_config.py
@@ -1,4 +1,63 @@
-# This file serves as a helper function file for the scatterplot generation.
+'''
+Contains config values for plot generation.
+'''
+network_plot_config = dict(
+    trace1=dict(
+        mode='lines',
+        line=dict(color='rgb(180,155,191)', width=1),
+        hoverinfo='none'
+    ),
+    trace2=dict(
+        mode='markers',
+        name='billionaires',
+        marker=dict(
+            symbol='circle',
+            size=6,
+            #  colorscale='Viridis',
+            autocolorscale=True,
+            line=dict(color='rgb(50,50,50)', width=0.5)
+        ),
+        hoverinfo='text'
+    )
+)
+
+network_layout_config = dict(
+    title="Links between 150 most viewed American billionaires' Wikipedia pages",
+    title_font_color='rgb(255,255,255)',
+    width=1000,
+    height=800,
+    showlegend=False,
+    paper_bgcolor='rgb(22,16,25)',
+    margin=dict(t=50),
+    annotations=[
+        dict(
+            showarrow=False,
+            text='Note that warmer colors indicate a larger number of page views',
+            xref='paper',
+            yref='paper',
+            x=0,
+            y=0.1,
+            xanchor='left',
+            yanchor='bottom',
+            font=dict(
+                size=14,
+                color='rgb(255,255,255)'
+            )
+        )
+    ],
+    hovermode='closest'
+)
+
+scatter_plot_config = dict(
+    mode='markers',
+    marker_color='rgb(204,40,81)',
+    marker_size=4,
+    hovertemplate =
+        '<b>%{text}</b>'+
+        '<br><b>Links</b>: %{x}'+
+        '<br><b>Views</b>: %{y}<br><extra></extra>'
+)
+
 scatter_axis_config = dict(
     title_font=dict(
         # size=24,
